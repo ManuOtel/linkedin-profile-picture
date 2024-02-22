52bcaef57b5b06e3e03f3689193b462e00ed36e1
Emanuel-Ionut Otel <manuotel@yahoo.com>
2024-02-22 15:07:11 +0800

    setup update

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as f:
 
 setup(
     name='linkedin profile picture',  
-    version='0.0.1',
-    author="Shashank Deshpande",
-    author_email="shashankdeshpande18@gmail.com",
+    version='0.0.2',
+    author="Emanuel-Ionut Otel, original by Shashank Deshpande",
+    author_email="manuotel@gmail.com",
     description="Python package to crawl linkedin profile pictures using google custom search API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/shashankdeshpande/linkedin",
+    url="https://github.com/ManuOtel/linkedin-profile-picture",
     python_requires=">=3.6.0",
     packages=find_packages()
     )
